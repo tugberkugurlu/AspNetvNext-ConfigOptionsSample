0a47397823b2e609ca354688303ac348776a8da6
tugberkugurlu <tugberkugurlu@gmail.com>
2014-10-30 23:19:35 +0200

    getting the repository per-request lifecycle now
     - stopped getting the IFarticusRepository instance through the middleware
       ctor
     - getting the IFarticusRepository instance from the RequestServices
       inside the Invoke method.

--- src/Farticus/FarticusMiddleware.cs
+++ src/Farticus/FarticusMiddleware.cs
@@ -12,14 +12,12 @@ namespace Farticus
     {
         // This shouldn't be static as this middleware is constructed per pipeline lifetime.
         private readonly ILogger _logger;
-        private readonly IFarticusRepository _repository;
         private readonly FarticusOptions _options;
 
         // Get ILoggerFactory injected into this middleware as it was put inside the DI system
         // by Microsoft.AspNet.Hosting at early points during the bootstrapping process.
         public FarticusMiddleware(RequestDelegate next,
             ILoggerFactory loggerFactory,
-            IFarticusRepository repository,
             IOptions<FarticusOptions> options)
         {
             if(loggerFactory == null)
@@ -32,23 +30,26 @@ namespace Farticus
                 throw new ArgumentNullException("options");
             }
 
-            if(repository == null)
-            {
-                throw new ArgumentNullException("repository");
-            }
-
             _logger = loggerFactory.Create(typeof(FarticusMiddleware).FullName);
-            _repository = repository;
             _options = options.Options;
         }
 
         public async Task Invoke(HttpContext context)
         {
+            IFarticusRepository repository = context
+                .RequestServices
+                .GetService(typeof(IFarticusRepository)) as IFarticusRepository;
+
+            if(repository == null)
+            {
+                throw new InvalidOperationException("IFarticusRepository is not available.");
+            }
+
             var builder = new StringBuilder();
             builder.Append("<div><strong>Farting...</strong></div>");
             for(int i = 0; i < _options.NumberOfMessages; i++)
             {
-                string message = await _repository.GetFartMessageAsync();
+                string message = await repository.GetFartMessageAsync();
                 builder.AppendFormat("<div>{0}</div>", message);
             }
 
